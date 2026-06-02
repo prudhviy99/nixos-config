ef161d50265df602eeca0339da0c17646d8c88cb
Prudhvi Yalamanchili <prudhviy99@gmail.com>
2026-06-02 03:05:26 -0700

    update CPU performance on battery

--- hosts/t14s/default.nix
+++ hosts/t14s/default.nix
@@ -30,7 +30,10 @@
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
       CPU_ENERGY_PERF_POLICY_ON_AC  = "performance";
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_performance";
+
+      CPU_BOOST_ON_AC  = 1;
+      CPU_BOOST_ON_BAT = 1;   # TLP disables turbo on battery by default
 
       # ThinkPad battery thresholds: charge to 80, start charging again at 75.
       # Massively extends battery lifespan over the years.
