f72a4665f22900a35df2137bfa3ee3df739bb0ce
Prudhvi Yalamanchili <prudhviy99@gmail.com>
2026-06-05 19:19:09 -0700

    git auth

--- home/git.nix
+++ home/git.nix
@@ -1,24 +1,37 @@
 { config, pkgs, ... }:
+
 {
   programs.git = {
     enable = true;
-    ignores = [ ".DS_Store" "*.swp" ".direnv/" "result" "result-*" ];
+    
+    # These attributes are natively supported at the top level
+    userName = "Prudhvi Yalamanchili";
+    userEmail = "prudhviy99@gmail.com";
+    
+    ignores = [ 
+      ".DS_Store" 
+      "*.swp" 
+      ".direnv/" 
+      "result" 
+      "result-*" 
+    ];
 
-    settings = {
-      user.name  = "Prudhvi Yalamanchili";
-      user.email = "prudhviy99@gmail.com";
+    aliases = {
+      st = "status";
+      co = "checkout";
+      br = "branch";
+      lg = "log --oneline --graph --decorate -20";
+    };
 
+    # Custom options go into extraConfig using Nix's dot syntax
+    extraConfig = {
       init.defaultBranch = "main";
       pull.rebase = true;
       push.autoSetupRemote = true;
       core.editor = "nvim";
-
-      alias = {
-        st = "status";
-        co = "checkout";
-        br = "branch";
-        lg = "log --oneline --graph --decorate -20";
-      };
+      
+      # This fixes your initial authentication problem securely using the GitHub CLI
+      credential.helper = "${pkgs.gh}/bin/gh auth git-credential";
     };
   };
 }
